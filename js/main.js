cb07f8d71bc159c48b412f782f39701dac41a226
Bourikerss <maxance.genestine@gmail.com>
2025-11-14 21:57:03 +0100

    Fix string splitting syntax for titles

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Cest pour toi :)').split('')
+    const titles = 'Cest pour toi :)'.split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
